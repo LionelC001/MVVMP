69b83ba334786301479110cbe6b70f88b2f4d4b3
l10nel <l10ne1>
2019-08-11 17:55:52 +0800

    feat(feature fragments): init

---
+++ app/src/main/java/com/lionel/mvvm_lifedata_navigation_realmp/features/feature1/Feature1Fragment.java
@@ -0,0 +1,37 @@
+package com.lionel.mvvm_lifedata_navigation_realmp.features.feature1;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.lionel.mvvm_lifedata_navigation_realmp.R;
+
+
+public class Feature1Fragment extends Fragment {
+
+    public Feature1Fragment() {
+
+    }
+
+    public static Feature1Fragment newInstance() {
+        Feature1Fragment fragment = new Feature1Fragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_feature1, container, false);
+    }
+}
---
+++ app/src/main/java/com/lionel/mvvm_lifedata_navigation_realmp/features/feature3/Feature3Fragment.java
@@ -0,0 +1,33 @@
+package com.lionel.mvvm_lifedata_navigation_realmp.features.feature3;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.lionel.mvvm_lifedata_navigation_realmp.R;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Feature3Fragment extends Fragment {
+
+
+    public Feature3Fragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_feature3, container, false);
+    }
+
+}
