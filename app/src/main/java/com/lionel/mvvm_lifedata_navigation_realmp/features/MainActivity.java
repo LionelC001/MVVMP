89a498a55bed2a908d467c786b51cbaef37dd179
l10nel <l10ne1>
2019-08-11 15:08:52 +0800

    fix(init): init

---
+++ app/src/main/java/com/lionel/mvvm_lifedata_navigation_realmp/features/MainActivity.java
@@ -0,0 +1,15 @@
+package com.lionel.mvvm_lifedata_navigation_realmp.features;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.lionel.mvvm_lifedata_navigation_realmp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
