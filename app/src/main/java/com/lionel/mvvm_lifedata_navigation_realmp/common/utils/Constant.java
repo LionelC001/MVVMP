8e36edf7506f58768209ae83c7b1e142177d3a61
l10nel <l10ne1>
2019-08-17 18:25:33 +0800

    feat(feature2-1): date text

--- app/src/main/java/com/lionel/mvvm_lifedata_navigation_realmp/common/utils/Constant.java
+++ app/src/main/java/com/lionel/mvvm_lifedata_navigation_realmp/common/utils/Constant.java
@@ -7,4 +7,8 @@ public final class Constant {
 
     //API_URL
     public static final String GIT_HUB_URL = BuildConfig.DOMAIN1;
+
+    //Date format
+    public static final String YYYY_MM_DD_T_HH_MM_SS_Z = "yyyy-MM-dd'T'hh:mm:ss'Z'";
+    public static final String YYYY_MM_DD_HH_MM_SS = "yyyy-MM-dd hh:mm:ss";
 }
--- app/src/main/java/com/lionel/mvvm_lifedata_navigation_realmp/common/utils/DataBindingAdapterHelper.java
+++ app/src/main/java/com/lionel/mvvm_lifedata_navigation_realmp/common/utils/DataBindingAdapterHelper.java
@@ -1,6 +1,7 @@
 package com.lionel.mvvm_lifedata_navigation_realmp.common.utils;
 
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.databinding.BindingAdapter;
 
@@ -8,10 +9,31 @@ import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
 import com.bumptech.glide.request.RequestOptions;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import static com.lionel.mvvm_lifedata_navigation_realmp.common.utils.Constant.YYYY_MM_DD_HH_MM_SS;
+import static com.lionel.mvvm_lifedata_navigation_realmp.common.utils.Constant.YYYY_MM_DD_T_HH_MM_SS_Z;
+
 public class DataBindingAdapterHelper {
 
     @BindingAdapter({"setImageWithGlide"})
     public static void setImageWithGlide(ImageView view, String url) {
         Glide.with(view).load(url).apply(RequestOptions.bitmapTransform(new RoundedCorners(20))).into(view);
+    }
+
+    @BindingAdapter({"setFormattedDate"})
+    public static void setFormattedDate(TextView view, String strDate) {
+        try {
+            SimpleDateFormat sdfOrigin = new SimpleDateFormat(YYYY_MM_DD_T_HH_MM_SS_Z, Locale.TAIWAN);
+            Date date = sdfOrigin.parse(strDate);
+            SimpleDateFormat sdfFormatted = new SimpleDateFormat(YYYY_MM_DD_HH_MM_SS, Locale.TAIWAN);
+            String strFormatted = sdfFormatted.format(date);
+            view.setText(strFormatted);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
     }
 }
