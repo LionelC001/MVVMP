7e578d197df6333d62b49f3d1bb378fac91ce734
l10nel <l10ne1>
2019-08-18 19:05:13 +0800

    style(feature2_1ViewModel): rename

--- app/src/main/java/com/lionel/mvvm_lifedata_navigation_realmp/features/feature2/Feature2_1ViewModel.java
+++ app/src/main/java/com/lionel/mvvm_lifedata_navigation_realmp/features/feature2/Feature2_1ViewModel.java
@@ -7,17 +7,14 @@ import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.Transformations;
-import androidx.paging.LivePagedListBuilder;
 import androidx.paging.PagedList;
 
 import com.lionel.mvvm_lifedata_navigation_realmp.common.model.response.GitHubResponse;
 
-import static com.lionel.mvvm_lifedata_navigation_realmp.features.feature2.Feature2_1DataSourceFactory.PAGE_SIZE;
-
 public class Feature2_1ViewModel extends AndroidViewModel {
-    private final MutableLiveData<String> keywordLiveData = new MutableLiveData<>();
+    private final MutableLiveData<String> mKeywordLiveData = new MutableLiveData<>();
     private final Feature2_1Repo mRepo;
-    private final LiveData<PagedList<GitHubResponse.ItemsBean>> mSearchPagedList = Transformations.switchMap(keywordLiveData, this::getPagedList);
+    private final LiveData<PagedList<GitHubResponse.ItemsBean>> mSearchPagedList = Transformations.switchMap(mKeywordLiveData, this::getPagedList);
 
 
     public Feature2_1ViewModel(@NonNull Application application) {
@@ -30,7 +27,7 @@ public class Feature2_1ViewModel extends AndroidViewModel {
     }
 
     public void performSearch(String keyword) {
-        keywordLiveData.setValue(keyword);
+        mKeywordLiveData.setValue(keyword);
     }
 
     private LiveData<PagedList<GitHubResponse.ItemsBean>> getPagedList(String input) {
