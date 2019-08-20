7f2a34f6c3f4030019e09d64a745d183cfce4acd
l10nel <l10ne1>
2019-08-20 23:12:39 +0800

    feat(MainActivity): badge at bottomNavigationBar
    init

--- app/src/main/java/com/lionel/mvvm_lifedata_navigation_realmp/features/main/Feature1Fragment.java
+++ app/src/main/java/com/lionel/mvvm_lifedata_navigation_realmp/features/main/Feature1Fragment.java
@@ -1,16 +1,25 @@
 package com.lionel.mvvm_lifedata_navigation_realmp.features.main;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.lionel.mvvm_lifedata_navigation_realmp.R;
+import com.lionel.mvvm_lifedata_navigation_realmp.common.callback.IBottomBadgeSetter;
+
+import java.util.Random;
 
 
 public class Feature1Fragment extends Fragment {
+
+    private IBottomBadgeSetter bottomBadgeSetter;
 
     public Feature1Fragment() {
 
@@ -24,6 +33,14 @@ public class Feature1Fragment extends Fragment {
     }
 
     @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        if (context instanceof IBottomBadgeSetter) {
+            bottomBadgeSetter = (IBottomBadgeSetter) context;
+        }
+    }
+
+    @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
@@ -33,5 +50,22 @@ public class Feature1Fragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_feature1, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        initView();
+    }
+
+    private void initView() {
+        Button btnBadge = getView().findViewById(R.id.btnBadge);
+        btnBadge.setOnClickListener(v -> refreshBadge());
+    }
+
+
+    private void refreshBadge() {
+        bottomBadgeSetter.refreshBadge((int) (Math.random() * 4), new Random().nextBoolean());
     }
 }
