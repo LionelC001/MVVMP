9c61b089ac559073603e065a95416c7eb0350514
l10nel <l10ne1>
2019-09-08 22:09:29 +0800

    fix(crop photo): prevent store photo cropped in gallery & recode

--- app/src/main/java/com/lionel/mvvm_lifedata_navigation_realmp/features/main/Feature3Fragment.java
+++ app/src/main/java/com/lionel/mvvm_lifedata_navigation_realmp/features/main/Feature3Fragment.java
@@ -2,6 +2,8 @@ package com.lionel.mvvm_lifedata_navigation_realmp.features.main;
 
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.provider.MediaStore;
@@ -23,10 +25,7 @@ import com.lionel.mvvm_lifedata_navigation_realmp.base.BaseFragment;
 import com.lionel.mvvm_lifedata_navigation_realmp.databinding.FragmentFeature3Binding;
 
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.util.List;
 
 import static android.app.Activity.RESULT_OK;
 
@@ -41,8 +40,7 @@ public class Feature3Fragment extends BaseFragment {
     private FragmentFeature3Binding dataBinding;
     private Button btnGo;
     private ImageView imgAvatar;
-    private Uri photoUri;
-    private Uri photoTakingUri;
+    private Uri uriAvatarOrigin;
     private File fileAvatarCrop;
 
     @Override
@@ -78,22 +76,28 @@ public class Feature3Fragment extends BaseFragment {
         Intent pickIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
         getIntent.setType("image/*");
         pickIntent.setType("image/*");
-        Intent chooserIntent = Intent.createChooser(getIntent, "Select Wallpaper");
+        Intent chooserIntent = Intent.createChooser(getIntent, "Select photo");
         chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, new Intent[]{pickIntent});
         startActivityForResult(chooserIntent, REQUEST_CODE_PICK_PHOTO);
     }
 
     private void takePhoto() {
-        File imagePhoto = new File(getActivity().getCacheDir(), "avatar_simple.png");
+        File imagePhoto = new File(getActivity().getCacheDir(), "avatar_origin.png");
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        photoTakingUri = FileProvider.getUriForFile(getActivity(), BuildConfig.APPLICATION_ID + ".provider", imagePhoto);
-        intent.putExtra(MediaStore.EXTRA_OUTPUT, photoTakingUri);
+        uriAvatarOrigin = FileProvider.getUriForFile(getActivity(), BuildConfig.APPLICATION_ID + ".provider", imagePhoto);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, uriAvatarOrigin);
         startActivityForResult(intent, REQUEST_CODE_TAKE_PHOTO);
     }
 
     private void cropPhoto() {
+        //產生圖片存的file位置
+        fileAvatarCrop = new File(getActivity().getCacheDir(), "avatar_crop.png");
+
+        //只要和外部應用程式溝通, 就最好用FileProvider將 file:// 轉成 content://  (for android 7.0 above)
+        Uri photoCropUri = FileProvider.getUriForFile(getActivity(), BuildConfig.APPLICATION_ID + ".provider", fileAvatarCrop);
+
         Intent cropIntent = new Intent("com.android.camera.action.CROP");
-        cropIntent.setDataAndType(photoTakingUri, "image/*");
+        cropIntent.setDataAndType(uriAvatarOrigin, "image/*");
         cropIntent.putExtra("crop", "true");
         cropIntent.putExtra("aspectX", 1);
         cropIntent.putExtra("aspectY", 1);
@@ -102,50 +106,40 @@ public class Feature3Fragment extends BaseFragment {
         cropIntent.putExtra("scale", "true");
         cropIntent.putExtra("outputFormat", Bitmap.CompressFormat.PNG.toString());
         cropIntent.putExtra("return-data", false);
-        cropIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        cropIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoCropUri);
+
+        // 似乎只要和fileProvider扯上關係, 就要宣告臨時權限
+        List<ResolveInfo> resInfoList = getActivity().getPackageManager().queryIntentActivities(cropIntent, PackageManager.MATCH_DEFAULT_ONLY);
+        for (ResolveInfo resolveInfo : resInfoList) {
+            String packageName = resolveInfo.activityInfo.packageName;
+            getActivity().grantUriPermission(packageName, uriAvatarOrigin, Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            getActivity().grantUriPermission(packageName, photoCropUri, Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+        }
+
         startActivityForResult(cropIntent, REQUEST_CODE_FOR_CROP_AVATAR);
     }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
         if (resultCode == RESULT_OK) {
             switch (requestCode) {
-
                 case REQUEST_CODE_PICK_PHOTO:
-                    photoTakingUri = data.getData();
+                    uriAvatarOrigin = data.getData();
                     cropPhoto();
                     break;
+
                 case REQUEST_CODE_TAKE_PHOTO:
                     cropPhoto();
                     break;
 
                 case REQUEST_CODE_FOR_CROP_AVATAR:
-                    if (data != null && data.getData() != null) {
-                        handleCropResult(data);
-                    }
+                    handleCropResult();
                     break;
             }
         }
     }
 
-    private void handleCropResult(Intent data) {
-        try {
-            fileAvatarCrop = new File(getActivity().getCacheDir(), "avatar_crop.png");
-            InputStream inputStream = getActivity().getContentResolver().openInputStream(data.getData());
-            byte[] buffer = new byte[inputStream.available()];
-            inputStream.read(buffer);
-            FileOutputStream fileOutputStream = new FileOutputStream(fileAvatarCrop);
-            fileOutputStream.write(buffer);
-
-            inputStream.close();
-            fileOutputStream.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
+    private void handleCropResult() {
         Glide.with(this)
                 .load(fileAvatarCrop)
                 .apply(RequestOptions.bitmapTransform(new CircleCrop()))
